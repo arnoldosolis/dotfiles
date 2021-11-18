627e206ba15eed998d565db73029ed7024462bc8
Arnoldo Solis <arnoldosolis817@gmail.com>
2021-11-18 08:44:09 -0500

    test

---
+++ nvim/lua/mappings.lua
@@ -0,0 +1,11 @@
+vim.cmd([[
+"""Nerd Tree"""
+nnoremap <leader>n :NERDTreeFocus<CR>
+nnoremap <C-n> :NERDTree<CR>
+nnoremap <C-t> :NERDTreeToggle<CR>
+nnoremap <C-f> :NERDTreeFind<CR>
+
+"""Conquer of Completion"""
+ 
+
+]])

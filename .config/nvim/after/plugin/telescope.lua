25c262a568b129bfe89fa0ef7126c3f2e637b1a0
Arnoldo Solis <asolis@rapptrlabs.com>
2022-02-05 20:53:12 -0500

    added telescope

--- .config/nvim/after/plugin/telescope.lua
+++ .config/nvim/after/plugin/telescope.lua
@@ -4,6 +4,7 @@ nnoremap <leader>ff <cmd>Telescope find_files<cr>
 nnoremap <leader>fg <cmd>Telescope live_grep<cr>
 nnoremap <leader>fb <cmd>Telescope buffers<cr>
 nnoremap <leader>fh <cmd>Telescope help_tags<cr>
+nnoremap <leader>gf <cmd>Telescope git_files<cr>
 ]])
 
 require('telescope').setup{
